368e2e58037ba23d7124a75d78f6b16ea5a50eea
Evelyn Nguyen <Evelyn@Viets-iMacx.local>
2020-07-08 18:42:35 -0700

    Episodes test done

---
+++ src/App.test.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { render} from "@testing-library/react";
+import App from "./App"
+
+test("page render with api", () => {
+
+    //arrange(render component and set up mock data)
+    render(<App />);
+
+
+    //act
+
+
+    //assert
+});
